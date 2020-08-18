f0538be1b86bdb106d2510e415132077c1138bcc
ilhamtuut <ilham.ahmad14@gmail.com>
2020-08-18 16:18:44 +0700

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         Schema::defaultStringLength(191);
-        // URL::forceScheme('https');
+        URL::forceScheme('https');
     }
 
     /**
