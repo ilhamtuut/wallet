2e64d5324fa3c1f4447044ea8dc7811f710bd581
ilhamtuut <ilham.ahmad14@gmail.com>
2020-08-28 16:46:04 +0700

    asd

--- resources/views/backend/wallet/index.blade.php
+++ resources/views/backend/wallet/index.blade.php
@@ -14,53 +14,53 @@
             </div>
             <div class="panel-body">
                 <div class="row">
-                    @if($address)
-                    <div class="col-md-6">
-                        <h3><b>Information Wallet</b></h3>
-                        <div class="table-responsive">
-                            <table class="table table-bordered">
-                                <tbody>
-                                    {{-- <tr>
-                                        <td width="50%">Total Transactions</td>
-                                        <td width="50%">0</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Total Received</td>
-                                        <td>0.00 GLP</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Total Sent</td>
-                                        <td>0.00 GLP</td>
-                                    </tr> --}}
-                                    <tr>
-                                        <td>Total Balance</td>
-                                        <td>{{$balance}} GLP</td>
-                                    </tr>
-                                </tbody>
-                            </table> 
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <h3><b>Account Settings</b></h3>
-                        <p>You can change your account settings like your email, password.</p>
-                        <a href="{{route('account.setting')}}" class="btn btn-primary mb">Change account settings</a>
-                        {{-- <h3><b>Backup</b></h3>
-                        <p>You can change your account settings like your email, password.</p>
-                        <a href="#" class="btn btn-primary">Paper Wallet</a> --}}
-                    </div>
-                    <div class="col-md-12">
-                        <h3><b>Your Wallet</b></h3>
-                        <div class="text-center">
-                            <img src="{{$qrCode}}" alt="qrcode">
-                            <p>Your GLP address</p>
-                            <h5><b style="line-break: anywhere;">{{$address}}</b></h5>
-                            <button type="button" class="btn btn-primary mb" onclick="copyToClipboard('{{$address}}')"><i class="fa fa-copy"></i> Copy Address</button>
-                            <p>You can deposit GLP to the address above to start using your online wallet.</p>
-                        </div>
-                    </div>
-                    @else
+                    @if($address == 'NULL')
                         <div class="col-md-12">
                             <a href="{{route('wallet.show_wallet')}}" class="btn btn-primary mb">Show Wallet</a>
+                        </div>
+                    @else
+                        <div class="col-md-6">
+                            <h3><b>Information Wallet</b></h3>
+                            <div class="table-responsive">
+                                <table class="table table-bordered">
+                                    <tbody>
+                                        {{-- <tr>
+                                            <td width="50%">Total Transactions</td>
+                                            <td width="50%">0</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Total Received</td>
+                                            <td>0.00 GLP</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Total Sent</td>
+                                            <td>0.00 GLP</td>
+                                        </tr> --}}
+                                        <tr>
+                                            <td>Total Balance</td>
+                                            <td>{{$balance}} GLP</td>
+                                        </tr>
+                                    </tbody>
+                                </table> 
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <h3><b>Account Settings</b></h3>
+                            <p>You can change your account settings like your email, password.</p>
+                            <a href="{{route('account.setting')}}" class="btn btn-primary mb">Change account settings</a>
+                            {{-- <h3><b>Backup</b></h3>
+                            <p>You can change your account settings like your email, password.</p>
+                            <a href="#" class="btn btn-primary">Paper Wallet</a> --}}
+                        </div>
+                        <div class="col-md-12">
+                            <h3><b>Your Wallet</b></h3>
+                            <div class="text-center">
+                                <img src="{{$qrCode}}" alt="qrcode">
+                                <p>Your GLP address</p>
+                                <h5><b style="line-break: anywhere;">{{$address}}</b></h5>
+                                <button type="button" class="btn btn-primary mb" onclick="copyToClipboard('{{$address}}')"><i class="fa fa-copy"></i> Copy Address</button>
+                                <p>You can deposit GLP to the address above to start using your online wallet.</p>
+                            </div>
                         </div>
                     @endif
                 </div>
