94d2807c9cebd7a19d49cf9b3ed8818bb052ddd5
ilhamtuut <ilham.ahmad14@gmail.com>
2021-01-30 10:02:35 +0700

    icon medsos

--- resources/views/layouts/_app.blade.php
+++ resources/views/layouts/_app.blade.php
@@ -1,6 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include('layouts.partials.header')
+    <style type="text/css">
+        .svg-inline--fa{
+            display: inline-block;
+            font-size: inherit;
+            height: 1em;
+            overflow: visible;
+            vertical-align: -0.125em;
+        }
+        .svg-inline--fa.fa-w-14{
+            width: 19.72px;
+        }
+    </style>
     <body>
         <!-- START PAGE CONTAINER -->
         <div class="page-container page-navigation-top page-navigation-top-custom">            
@@ -12,12 +24,14 @@
                     <a href="{{route('home')}}" class="logo"></a>
                     <div class="pull-right">                        
                         <div class="socials">
-                            <a href="#"><span class="fa fa-facebook-square"></span></a>
-                            <a href="#"><span class="fa fa-twitter-square"></span></a>
-                            <a href="#"><span class="fa fa-instagram"></span></a>
+                            <a href="https://www.facebook.com/greenlineprojectcoin/" target="_blank"><span class="fa fa-facebook-square"></span></a>
+                            <a href="https://twitter.com/coinglp" target="_blank"><span class="fa fa-twitter-square"></span></a>
+                            <a href="https://www.instagram.com/CoinGLP/" target="_blank"><span class="fa fa-instagram"></span></a>
+                            <a href="https://glpcoin.medium.com/" target="_blank"><span class="fa"><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="medium" class="svg-inline--fa fa-medium fa-w-14 fa-2x" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M0 32v448h448V32H0zm372.2 106.1l-24 23c-2.1 1.6-3.1 4.2-2.7 6.7v169.3c-.4 2.6.6 5.2 2.7 6.7l23.5 23v5.1h-118V367l24.3-23.6c2.4-2.4 2.4-3.1 2.4-6.7V199.8l-67.6 171.6h-9.1L125 199.8v115c-.7 4.8 1 9.7 4.4 13.2l31.6 38.3v5.1H71.2v-5.1l31.6-38.3c3.4-3.5 4.9-8.4 4.1-13.2v-133c.4-3.7-1-7.3-3.8-9.8L75 138.1V133h87.3l67.4 148L289 133.1h83.2v5z"></path></svg></span></a>
+                            <a href="https://t.me/greenlineproject" target="_blank"><span class="fa"><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="telegram" class="svg-inline--fa fa-telegram fa-w-14 fa-2x" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z"></path></svg></span></a>
                         </div>
                         <div class="contacts">
-                            <a href="#">Copyright © {{date('Y')}} Greenline Project. All rights reserved. </a>
+                            <a href="#">Copyright © {{date('Y')}} Greenline Project. All rights reserved.</a>
                         </div>
                     </div>
                 </div>
